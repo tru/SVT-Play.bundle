72f613ec70913b8c3f7e8b91ff4b374a1626a5f3
Coil <notvalid@whatnot.com>
2011-05-18 21:14:47 +0200

    added texts and urls for current and latest news

--- Contents/Code/common.py
+++ Contents/Code/common.py
@@ -15,6 +15,7 @@ URL_LIVE = "http://svtplay.se/?cb,a1364145,1,f,-1/pb,a1596757,1,f,"
 URL_RECOMMENDED_SHOWS = "http://svtplay.se/?cb,a1364145,1,f,-1/pb,a1364142,1,f,"
 URL_LATEST_CLIPS = "http://svtplay.se/?cb,a1364145,1,f,/pb,a1364142,1,f,-1"
 URL_LATEST_SHOWS = "http://svtplay.se/?cb,a1364145,1,f,-1/pb,a1364143,1,f,"
+URL_LATEST_NEWS = "http://svtplay.se/?cb,a1364145,1,f,-1/pb,a1527537,1,f,"
 URL_MOST_VIEWED = "http://svtplay.se/?cb,a1364145,1,f,-1/pb,a1364144,1,f,"
 URL_CATEGORIES = "http://svtplay.se/kategorier"
 URL_CAT_CHILD = "http://svtplay.se/c/96251/barn"
@@ -33,6 +34,7 @@ TEXT_PREFERENCES = u'Inställningar'
 TEXT_RECOMMENDED_SHOWS = u'Rekommenderat'
 TEXT_LATEST_CLIPS = u'Senaste klipp'
 TEXT_LATEST_SHOWS = u'Senaste program'
+TEXT_LATEST_NEWS = u'Senaste nyhetsprogram'
 TEXT_MOST_VIEWED = u'Mest sedda'
 TEXT_CATEGORIES = u'Kategorier'
 TEXT_CAT_CHILD = u'Barn'
@@ -43,6 +45,7 @@ TEXT_CAT_NEWS = u'Nyheter'
 TEXT_CAT_SPORT = u'Sport'
 
 TEXT_LIVE = u'LIVE: '
+TEXT_LIVE_CURRENT = u'JUST NU: '
 
 #The page step function will only step this many pages deep. Can be changed / function call.
 MAX_PAGINATE_PAGES = 5
